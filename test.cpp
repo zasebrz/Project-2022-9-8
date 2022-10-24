6f4391f18dd8d07bbc07de1731055931f1555140
zasebrz <baorongzhen@bupt.edu.cn>
2022-10-24 22:05:10 +0800

    2022年10月24日22:05:08

--- test.cpp
+++ test.cpp
@@ -22539,3 +22539,127 @@ T m_lcm(T a, T b)
 //    }
 //    return dp[n];
 //}
+
+/////////////////////////////////////////80. 删除有序数组中的重复项 II//////////////////////////
+
+//https://leetcode.cn/problems/remove-duplicates-from-sorted-array/solutions/575549/shua-chuan-lc-jian-ji-shuang-zhi-zhen-ji-2eg8/
+//对于此类问题，我们应该进行如下考虑：
+//由于是保留 k 个相同数字，对于前 k 个数字，我们可以直接保留。
+//对于后面的任意数字，能够保留的前提是：与 当前写入的位置 前面的第 k 个元素进行比较，不相同则保留。
+//因为数据是有序的，对于当前写入的位置来说，它前面的序列一定是符合条件的（相同的数最多保留 k 个）
+//因此，如果 j-k 位置上的元素等于 要插入的数，那么说明这个数已经被保留了 k 个（从 j-k 到 j-1总共 k 个）
+//则当前要插入的数 不能 在插入到当前位置了，继续遍历下一个数
+//举个例子，我们令 k = 1，假设有样例：[3, 3, 3, 3, 4, 4, 4, 5, 5, 5]
+//设定变量 j，指向待插入位置。由于前 k 个数直接保留，所以 j 初始值为 k(j 代表下标），目标数组为[3]
+//继续往后遍历，能够保留的前提是与 j 的前面 1 位元素不同（性质 2），因此我们会跳过剩余的 3，将第一个 4 追加进去。
+//j 变为 2，目标数组为[3, 4]
+//继续这个过程，跳过剩余的 4，将第一个 5 追加进去。j 变为 3，目标数组为[3, 4, 5]
+//当整个数组被扫描完，最终我们得到了目标数组[3, 4, 5] 和 答案 j 为 3。
+//如下题
+//int process(vector<int>& nums, int k) {
+//    int j = k;
+//    for (int i = k;i < nums.size();i++)
+//    {
+//        if (nums[i] != nums[j - k])
+//        {
+//            nums[j] = nums[i];
+//            j++;
+//        }
+//    }
+//    return j;
+//}
+//int main()
+//{
+//    vector<int> nums = { 0, 0, 1, 1, 1, 1, 2, 3, 3 };
+//    int k = 2;
+//    cout << process(nums, k) << endl;  
+//}
+
+/////////////////////////////////////////2448. 使数组相等的最小开销//////////////////////////
+
+//https://leetcode.cn/problems/minimum-cost-to-make-array-equal/solutions/1917421/by-endlesscheng-i10r/
+//将 nums 和 cost 绑在一起排序。
+//首先计算使所有元素都变成 nums[0] 的总开销 total，以及所有 cost 的和 sumCost。
+//然后考虑要使所有元素都等于 nums[1]，total 的变化量是多少：
+//有 cost[0] 这么多的开销要 增加 nums[1]−nums[0]（我们要让nums[0]变成nums[1]，代价是cost[0]）；
+//有 sumCost−cost[0] 这么多的开销要 减少 nums[1]−nums[0]（现在所有值都变成nums[0]了，但是nums[1]比nums[0]大，
+//那么nums[2]，nums[3]这些值变成nums[1]的代价就要减少，我们要让除了nums[0]之外的所有数变成nums[1]，代价是sumCost−cost[0]）
+//因此 total 减少了
+//(sumCost−2⋅cost[0])⋅(nums[1]−nums[0]) 
+//按照这个公式模拟后续 nums[i]，取所有 total 最小值为答案。
+//int main()
+//{
+//    vector<int> nums = { 1, 3, 5, 2 }, cost = { 2, 3, 1, 14 };
+//    int n = nums.size();
+//    vector<pair<int, int>> a(n);
+//    for (int i = 0; i < n; ++i) {
+//        a[i] = { nums[i], cost[i] };
+//    }
+//    sort(a.begin(), a.end());//按照nums从小到大排序
+//    long long total = 0, sumCost = 0;
+//    for (int i = 0; i < n; ++i) {
+//        total += (long long)(a[i].first - a[0].first) * a[i].second;//初始化total
+//        sumCost += a[i].second;//初始化sumCost
+//    }
+//    long long ans = total;
+//    for (int i = 1; i < n; ++i) {
+//        sumCost -= a[i - 1].second * 2;
+//        total -= sumCost * (a[i].first - a[i - 1].first);//重新计算total
+//        ans = min(ans, total);
+//    }
+//    cout<<ans;
+//}
+//*******************************************中位数法**********************************************
+//把 cost[i] 理解成 nums[i] 的出现次数，因为把一个数加减 1 的代价 cost [i] 就等于把 cost[i]个数加减 1 的代价
+//（此时每个数加减 1 的代价为1）,根据中位数贪心，把所有数变成中位数是最优的。
+//详细证明参考 462. 最小操作次数使数组元素相等 II。
+//代码实现时，仍然按照方法一那样排序，然后不断累加 cost[i]，首次累加到 大于等于 sumCost2/2​ 时就找到了中位数。
+//由于 sumCost 可能是奇数，所以要上取整，即首次累加到 大于等于 sumCost2 / 2​​ 时就找到了中位数。
+//https://leetcode.cn/problems/minimum-moves-to-equal-array-elements-ii/solutions/1504085/by-fuxuemingzhu-13z3/
+//https://www.sohu.com/a/226264278_553401
+//int main()
+//{
+//    vector<int> nums = { 1, 3, 5, 2 }, cost = { 2, 3, 1, 14 };
+//    int n = nums.size();
+//    long long sumCost = 0;
+//    vector<pair<int, int>> a(n);
+//    for (int i = 0; i < n; ++i) {
+//        a[i] = {nums[i], cost[i]};
+//        sumCost += cost[i];//此时cost[i]就相当于变换后数组里每个数的个数，sumCost就是变换后数组里数的个数
+//    }
+//    sort(a.begin(), a.end());//还是按照从小到大排序
+//    //找中位数其实就是找 sumCost/2，但是由于sumCost不是逐个增加的，而是跳变的，所以当 前一个累加的s小于mid，而当前累加的s
+//    //大于等于mid，就说明中位数就是当前这个数 x1
+//    long long ans = 0, s = 0, mid = (sumCost) / 2;
+//    for (auto& [x1, c1] : a) 
+//    {
+//        s += c1;//累加个数，找中位数
+//        if (s >= mid) 
+//        {//说明中位数出现就是当前的 x1
+//            for (auto& [x2, c2] : a) {
+//                ans += (long long)abs(x2 - x1) * c2;//x1就是我们要找的中位数，计算把所有数变成 x1 的代价
+//            }
+//            break;
+//        }
+//    }
+//    return ans;
+//}
+
+//输入：nums = [1, 2, 5], target = [4, 1, 3]
+//输出：1
+//解释：一步操作可以使 nums 变得与 target 相似：
+//- 选择 i = 1 和 j = 2 ，nums = [1, 4, 3] 。
+int main()
+{
+    vector<int> nums = { 1, 2, 5 }, target = { 4, 1, 3 };
+    sort(nums.begin(), nums.end());
+    sort(target.begin(), target.end());
+    long long ans = 0L;
+    int js[2]{}; // 用数组表示两个下标，这样不用讨论奇偶性
+    for (int x : nums) {
+        int p = x % 2, & j = js[p];
+        while (target[j] % 2 != p) ++j; // 找 target 中奇偶性相同的元素
+        ans += abs(x - target[j++]);
+    }
+    return ans / 4;
+}
