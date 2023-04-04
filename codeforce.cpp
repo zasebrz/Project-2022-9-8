4d29e57aa464f99b9d3fa7546040607c96890027
zasebrz <baorongzhen@bupt.edu.cn>
2023-04-04 11:13:07 +0800

    2023年4月4日11:13:04

---
+++ codeforce.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+#include<array>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<unordered_set>
+#include<unordered_map>
+#include<stdio.h>
+#include<time.h>
+#include<string>
+#include<sstream>
+#include<fstream>
+#include<unordered_map>
+#include<stack>
+#include<numeric>
+#include<tuple>
+#include <assert.h>
+#include<time.h>
+#include<Windows.h>
+#include <functional>
+#include <assert.h>
+#include<thread>
+#include<mutex>
+#include<forward_list>
+#include<bitset>
+#include<regex>
+#include<random>
+#include<cmath>
+#include <iomanip> // 包含头文件
+using namespace std;
+
+//void solve()
+//{
+//    long long n, x, p;
+//    cin >> n >> x >> p;
+//    for (long long i = 1; i <= min(p, 2 * n); ++i)
+//    {
+//        if ((x + i * (i + 1) / 2) % n == 0)
+//        {
+//            cout << "Yes" << endl;
+//            return;
+//        }
+//    }
+//    cout << "No" << endl;
+//}
+//int main()
+//{
+//    int t;
+//    cin >> t;
+//    while (t--)
+//    {
+//        solve();
+//    }
+//}
